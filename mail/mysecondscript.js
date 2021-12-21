559b76aa0f533002e0de3d6beda5b04ab749b5de
Alessio Tiberi <alessio.1997.18.01@gmail.com>
2021-12-21 10:55:29 +0100

    finish

---
+++ mail/mysecondscript.js
@@ -0,0 +1,23 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let utente = Math.floor(Math.random()*6+1);
+
+let computer = Math.floor(Math.random()*6+1);
+
+document.getElementById("utent").innerHTML = `numero il tuo numero è:  ${utente}`;
+document.getElementById("comp").innerHTML = `numero del computer è :  ${computer}`;
+
+
+if(utente > computer){
+   
+    document.getElementById("vincitore").innerHTML = "complimenti hai vinto!!";
+
+}else if(utente == computer){
+
+    document.getElementById("vincitore").innerHTML = "pareggio -.-";
+
+}else{
+    
+    document.getElementById("vincitore").innerHTML = "ci dispiace, hai perso :(";
+   
+}
